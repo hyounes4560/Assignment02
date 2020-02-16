96429264d1a4e77392628735baf9e055b84a4698
hyounes4560 <hyounes4560@conestogac.on.ca>
2020-02-16 01:11:35 -0500

    setting up the test project

--- Assignment02.Tests/TriangleSolverTest.cs
+++ Assignment02.Tests/TriangleSolverTest.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assignment02;
+using NUnit.Framework;
 
 namespace Assignment02.Tests
 {
+
+    [TestFixture]
     class TriangleSolverTest
     {
+
+        [TestCase]
+        public void Analyze_Input3and3and3_ReturnEquilateralTriangle()
+        {
+
+            string result = TriangleSolver.Analyze(3, 3, 3);
+
+            StringAssert.DoesNotContain("Equilateral triangle", result);
+
+        }
+
+
     }
 }
