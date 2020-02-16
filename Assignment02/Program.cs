308735a055423961d41da0c1e422359c5e8a1901
hyounes4560 <hyounes4560@conestogac.on.ca>
2020-02-15 23:29:32 -0500

    implemented the TraingleSolver class with the Analyze method

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -8,8 +8,104 @@ namespace Assignment02
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+
+            int firstSide;
+            int secondSide;
+            int thirdSide;
+            string sideString = "";
+            string option = "";
+            bool flag = false;
+
+            try
+            {
+
+
+
+                do
+                {
+
+                    do
+                    {
+                        Console.WriteLine("\nChoose one option from the following two options:");
+                        Console.WriteLine("1.Enter triangle dimensions\n" +
+                                           "2.Exit\n");
+
+                        option = Console.ReadLine();
+
+                    } while (!int.TryParse(option, out int userOption) || (userOption < 1 || userOption > 2));
+
+                    //to keep showing the main menu 
+                    flag = true;
+
+                    switch (option)
+                    {
+                        case "1":
+
+                            do
+                            {
+                                Console.WriteLine("Please enter the first side of the traingle");
+                                sideString = Console.ReadLine();
+
+                            } while ((!int.TryParse(sideString, out firstSide) || firstSide < 1));
+
+                            do
+                            {
+                                Console.WriteLine("Please enter the second side of the traingle");
+                                sideString = Console.ReadLine();
+
+                            } while ((!int.TryParse(sideString, out secondSide) || secondSide < 1));
+
+                            do
+                            {
+
+                                Console.WriteLine("Please enter the third side of the traingle");
+                                sideString = Console.ReadLine();
+
+                            } while ((!int.TryParse(sideString, out thirdSide) || thirdSide < 1));
+
+
+
+
+                            string result = TriangleSolver.Analyze(firstSide, secondSide, thirdSide);
+                            Console.WriteLine(result);
+                            Console.ReadKey();
+
+                            break;
+
+
+                        case "2":
+                            Console.WriteLine("Sorry to see you leave! GoodBye....");
+
+                            //to stop showing the main menu and exit the application
+                            flag = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("Something went wrong");
+                            break;
+
+                    }
+                } while (flag);
+
+
+
+
+            }
+            catch
+            {
+
+                throw new NotImplementedException();
+
+            }
+
+
+
+
+
         }
     }
 }
---
+++ Assignment02/TriangleSolver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public static class TriangleSolver
+    {
+
+
+        public static String Analyze(int firstSide, int secondSide, int thirdSide)
+        {
+
+
+            if ((firstSide + secondSide) > thirdSide && (firstSide + thirdSide) > secondSide && (secondSide + thirdSide) > firstSide)
+            {
+                string result;
+
+                if (firstSide == secondSide && firstSide == thirdSide)
+                {
+
+                    result = "The traingle they form is an Equilateral triangle.\n";
+                }
+
+                else if (firstSide == secondSide || firstSide == thirdSide || secondSide == thirdSide)
+                {
+
+                    result = "The traingle they form is an Isosceles traingle.\n";
+                }
+
+                else
+                {
+                    result = "The traingle they form is a Scalene traingle.\n";
+
+                }
+
+                return "The three sides you entered form a traingle.\n" + result + "Press any key to go back to the main menu and try again.";
+
+            }
+
+
+            else { return "The three sides you entered don't form a traingle!\nPress any key to go back to the main menu and try again."; }
+
+
+
+
+        }
+
+    }
+}
